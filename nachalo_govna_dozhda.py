87eb0fbe7f92ee8d5adc620f719daca4b40c3b04
levon <airtk@ya.ru>
2021-01-05 12:12:08 +0300

    какащка

--- nachalo_govna_dozhda.py
+++ nachalo_govna_dozhda.py
@@ -1,14 +1,16 @@
-import pygame,time
+import pygame,time,p_o_m_o_s_h
 
 speedx = 10
 speedy = 10
 pop = [600, 800]
 e = pygame.display.set_mode(pop)
 
-kakashka_ubiyca = pygame.Rect(0,700,600,100)
-w = pygame.Rect(200, -200, 200, 200)
-pol=pygame.image.load("govno_kartinke/kk.jpeg")
-pul=pygame.image.load("govno_kartinke/ff.jpeg")
+kakashka_ubiyca = pygame.Rect(0, 800, 600, 100)
+kaka_1 = pygame.Rect(450, -200, 150,150)
+kaka_2 = pygame.Rect(150,-200,150, 150)
+kartinka_kaki=pygame.image.load("govno_kartinke/kk.jpeg")
+kartinka_kaki=p_o_m_o_s_h.izmeni_kartinku(kartinka_kaki,150,150,[255,255,255],10)
+# kartinka_unitaza=pygame.image.load("govno_kartinke/ff.jpeg")
 while 1 == 1:
     time.sleep(1/60)
     # управление
@@ -17,18 +19,26 @@ while 1 == 1:
         if s.type == pygame.QUIT:
             exit()
 
-    # движение
-    w.y = w.y + speedy
-    if w.bottom > 800:
-        w.y=0
+    # движение kaki 1
+    kaka_1.y = kaka_1.y + speedy+5
+    if kaka_1.bottom > 800:
+        kaka_1.y=0
         kakashka_ubiyca.y-=10
         kakashka_ubiyca.h+=10
-
+    # движение kaki 2
+    kaka_2.y = kaka_2.y + speedy
+    if kaka_2.bottom > 800:
+        kaka_2.y = 0
+        kakashka_ubiyca.y -= 10
+        kakashka_ubiyca.h += 10
     # рисование
-    e.blit(pol,w)
-    e.blit(pul,kakashka_ubiyca)
+    e.blit(kartinka_kaki, kaka_1)
+    e.blit(kartinka_kaki,kaka_2)
+    # e.blit(kartinka_unitaza, kakashka_ubiyca)
 
     # pygame.draw.rect(e, [255, 155, 55], w)
-    # pygame.draw.rect(e, [110,64, 21], kakashka_ubiyca)
+    # pygame.draw.rect(e, [110,64, 21], kaka_2)
+
+    pygame.draw.rect(e, [110,64, 21],kakashka_ubiyca)
     pygame.display.flip()
     e.fill([44,44,44])
