2cf494ca02efe9b07b310285c7444a310577df3e
levon <airtk@ya.ru>
2021-01-07 12:07:57 +0300

    сделалли тулет частично

--- nachalo_govna_dozhda.py
+++ nachalo_govna_dozhda.py
@@ -1,4 +1,4 @@
-import pygame,time,p_o_m_o_s_h
+import pygame, time, p_o_m_o_s_h
 
 speedx = 10
 speedy = 10
@@ -6,39 +6,47 @@ pop = [600, 800]
 e = pygame.display.set_mode(pop)
 
 kakashka_ubiyca = pygame.Rect(0, 800, 600, 100)
-kaka_1 = pygame.Rect(450, -200, 150,150)
-kaka_2 = pygame.Rect(150,-200,150, 150)
-kartinka_kaki=pygame.image.load("govno_kartinke/kk.jpeg")
-kartinka_kaki=p_o_m_o_s_h.izmeni_kartinku(kartinka_kaki,150,150,[255,255,255],10)
+kaka_1 = pygame.Rect(450, -200, 150, 150)
+kaka_2 = pygame.Rect(150, -200, 150, 150)
+tualet = pygame.Rect(300, 600, 200, 75)
+kartinka_tualeta = pygame.image.load("govno_kartinke/ff.jpeg")
+kartinka_kaki = pygame.image.load("govno_kartinke/kk.jpeg")
+kartinka_kaki = p_o_m_o_s_h.izmeni_kartinku(kartinka_kaki, 150, 150, [255, 255, 255], 10)
+kartinka_tualeta = p_o_m_o_s_h.izmeni_kartinku(kartinka_tualeta, 150, 150, [16, 16, 16], 10)
+
 # kartinka_unitaza=pygame.image.load("govno_kartinke/ff.jpeg")
 while 1 == 1:
-    time.sleep(1/60)
+    time.sleep(1 / 60)
     # управление
     u = pygame.event.get()
     for s in u:
         if s.type == pygame.QUIT:
             exit()
 
+        if s.type == pygame.KEYDOWN and s.key == pygame.K_a:
+            tualet.x = tualet.x - 10
+
+        if s.type == pygame.KEYDOWN and s.key == pygame.K_d:
+            tualet.x = tualet.x + 10
+
     # движение kaki 1
-    kaka_1.y = kaka_1.y + speedy+5
-    if kakashka_ubiyca.y<kaka_1.bottom:
-        kaka_1.y=0
-        kakashka_ubiyca.y-=10
-        kakashka_ubiyca.h+=10
+    kaka_1.y = kaka_1.y + speedy + 5
+    if kakashka_ubiyca.y < kaka_1.bottom:
+        kaka_1.y = 0
+        kakashka_ubiyca.y -= 10
+        kakashka_ubiyca.h += 10
     # движение kaki 2
     kaka_2.y = kaka_2.y + speedy
-    if kakashka_ubiyca.y<kaka_2.bottom:
+    if kakashka_ubiyca.y < kaka_2.bottom:
         kaka_2.y = 0
         kakashka_ubiyca.y -= 10
         kakashka_ubiyca.h += 10
     # рисование
     e.blit(kartinka_kaki, kaka_1)
-    e.blit(kartinka_kaki,kaka_2)
-    # e.blit(kartinka_unitaza, kakashka_ubiyca)
+    e.blit(kartinka_kaki, kaka_2)
+    e.blit(kartinka_tualeta, tualet)
 
-    # pygame.draw.rect(e, [255, 155, 55], w)
-    # pygame.draw.rect(e, [110,64, 21], kaka_2)
 
-    pygame.draw.rect(e, [110,64, 21],kakashka_ubiyca)
+    pygame.draw.rect(e, [110, 64, 21], kakashka_ubiyca)
     pygame.display.flip()
-    e.fill([44,44,44])
+    e.fill([44, 44, 44])
